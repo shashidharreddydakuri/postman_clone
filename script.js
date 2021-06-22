aa5a86f19ecb97de07fc8de1b34210d25ffbcff9
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2021-06-22 14:43:30 +0530

    functionality working

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const queryParamsContainer = document.querySelector('[data-query-params]');
+const requestHeadersContainer = document.querySelector(
+	'[data-request-headers]'
+);
+const keyValueTemplate = document.querySelector('[data-key-value-template]');
+
+document
+	.querySelector('[data-add-query-param-btn]')
+	.addEventListener('click', () => {
+		queryParamsContainer.append(createKeyValuePair());
+	});
+
+document
+	.querySelector('[data-add-request-header-btn]')
+	.addEventListener('click', () => {
+		requestHeadersContainer.append(createKeyValuePair());
+	});
+
+queryParamsContainer.append(createKeyValuePair());
+requestHeadersContainer.append(createKeyvaluePair());
+
+function createKeyValuePair() {
+	const element = keyValueTemplate.content.cloneNode(true);
+	element.querySelector('[data-remove-btn]').addEventListener('click', (e) => {
+		e.target.closest('[data-key-value-pair]').remove();
+	});
+	return element;
+}
