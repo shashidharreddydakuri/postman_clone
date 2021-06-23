c8111017bc2111df57f870a3c9eaf87391899b80
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2021-06-24 04:33:58 +0530

    json editor added and basic api client completed

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import prettyBytes from 'pretty-bytes';
+import setupEditors from './setupEditor';
 
 const form = document.querySelector('[data-form]');
-
 const queryParamsContainer = document.querySelector('[data-query-params]');
 const requestHeadersContainer = document.querySelector(
 	'[data-request-headers]'
 );
 const keyValueTemplate = document.querySelector('[data-key-value-template]');
-const responseHeadersContainer = document.querySelector('[data-response-headers]');
+const responseHeadersContainer = document.querySelector(
+	'[data-response-headers]'
+);
 
 document
 	.querySelector('[data-add-query-param-btn]')
@@ -26,49 +29,65 @@ document
 queryParamsContainer.append(createKeyValuePair());
 requestHeadersContainer.append(createKeyValuePair());
 
-axios.interceptors.request.use(request => {
-	request.customData = request.customData || {}
-	request.customData.startTime = new Date().getTime()
-	return request
-})
+axios.interceptors.request.use((request) => {
+	request.customData = request.customData || {};
+	request.customData.startTime = new Date().getTime();
+	return request;
+});
 
 function updateEndTime(response) {
-	response.customData = response.customData || {}
-	response.customDta.time = new Date().getTime() - response.config.customData.startTime
-	return response
+	response.customData = response.customData || {};
+	response.customData.time =
+		new Date().getTime() - response.config.customData.startTime;
+	return response;
 }
 
-axios.interceptors.response.use(updateEndTime, e => {
-	Promise.reject(updateEndTime(e.response))
-})
+axios.interceptors.response.use(updateEndTime, (e) => {
+	return Promise.reject(updateEndTime(e.response));
+});
 
+const { requestEditor, updateResponseEditor } = setupEditors();
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
+
+	let data;
+	try {
+		data = JSON.parse(requestEditor.state.doc.toString() || null);
+	} catch (e) {
+		alert('JSON data is malformed');
+		return;
+	}
 
 	axios({
 		url: document.querySelector('[data-url]').value,
 		method: document.querySelector('[data-method]').value,
 		params: keyValuePairsToObjects(queryParamsContainer),
 		headers: keyValuePairsToObjects(requestHeadersContainer),
+		data,
 	})
-	.catch(e => e)
-	.then(response => {
-		document
-			.querySelector('[data-response-section]')
-			.classList.remove('d-none');
-			updateResponseDetails(response)
-			// updateResponseEditor(response)
-			updateResponseHeaders(response.headers)
-		console.log(response)
-	})
+		.catch((e) => e)
+		.then((response) => {
+			document
+				.querySelector('[data-response-section]')
+				.classList.remove('d-none');
+			updateResponseDetails(response);
+			updateResponseEditor(response.data);
+			updateResponseHeaders(response.headers);
+			console.log(response);
+		});
 });
 
 function updateResponseDetails(response) {
-	document.querySelector('[data-status]').textContent = response.status
+	document.querySelector('[data-status]').textContent = response.status;
+	document.querySelector('[data-time]').textContent = response.customData.time;
+	document.querySelector('[data-size]').textContent = prettyBytes(
+		JSON.stringify(response.data).length +
+			JSON.stringify(response.headers).length
+	);
 }
 
 function updateResponseHeaders(headers) {
-	responseHeadersContainer.innerHTML = ""
+	responseHeadersContainer.innerHTML = '';
 	Object.entries(headers).forEach(([key, value]) => {
 		const keyElement = document.createElement('div');
 		keyElement.textContent = key;
